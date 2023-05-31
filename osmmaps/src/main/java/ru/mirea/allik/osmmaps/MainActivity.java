25aa841d4a98aee96f2417a5c8fa406ea1efe4f3
Илья <iallik@mail.ru>
2023-06-01 02:54:00 +0300

    Initial commit

---
+++ osmmaps/src/main/java/ru/mirea/allik/osmmaps/MainActivity.java
@@ -0,0 +1,120 @@
+package ru.mirea.allik.osmmaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.PreferenceManager;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import ru.mirea.allik.osmmaps.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView = null;
+    private ActivityMainBinding binding;
+    boolean isWork;
+    private static final int REQUEST_CODE_PERMISSION = 100;
+    MyLocationNewOverlay locationNewOverlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mapView = binding.mapView;
+        mapView.setZoomRounding(true);
+        mapView.setMultiTouchControls(true);
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(15.0);
+        GeoPoint startPoint = new GeoPoint(55.794229, 37.700772);
+        mapController.setCenter(startPoint);
+        locationNewOverlay = new MyLocationNewOverlay(new
+                GpsMyLocationProvider(getApplicationContext()),mapView);
+        locationNewOverlay.enableMyLocation();
+        mapView.getOverlays().add(this.locationNewOverlay);
+        CompassOverlay compassOverlay = new CompassOverlay(getApplicationContext(), new
+                InternalCompassOrientationProvider(getApplicationContext()), mapView);
+        compassOverlay.enableCompass();
+        mapView.getOverlays().add(compassOverlay);
+        final Context context = this.getApplicationContext();
+        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        ScaleBarOverlay scaleBarOverlay = new ScaleBarOverlay(mapView);
+        scaleBarOverlay.setCentred(true);
+        scaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        mapView.getOverlays().add(scaleBarOverlay);
+
+        Marker marker = new Marker(mapView);
+        marker.setPosition(new GeoPoint(55.705001, 37.922335));
+        marker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"Место силы",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker);
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker.setTitle("Дом родной");
+
+        Marker marker2 = new Marker(mapView);
+        marker2.setPosition(new GeoPoint(55.794259, 37.701448));
+        marker2.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker2, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"Корпус (крепкий)",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker2);
+        marker2.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker2.setTitle("Стромынка");
+
+        Marker marker3 = new Marker(mapView);
+        marker3.setPosition(new GeoPoint(55.616905, 37.673016));
+        marker3.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker3, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"Я тут гулять люблю",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker3);
+        marker3.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker3.setTitle("Царицыно");
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Configuration.getInstance().save(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
+}
---
+++ yandexdriver/src/main/java/ru/mirea/allik/yandexdriver/App.java
@@ -0,0 +1,15 @@
+package ru.mirea.allik.yandexdriver;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "bda5a54f-65c1-4144-840a-017f60dd020b";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
